5f546c8530bce55e3a35a98c2314adac8cfd1cda
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-26 22:33:06 +0000

    DRY'd the tests for account instantiation

--- bank_tech_test/spec/account.spec.js
+++ bank_tech_test/spec/account.spec.js
@@ -1,31 +1,31 @@
 const Account = require('../lib/account.js')
 
 describe('account', () => {
+  let account
+  beforeEach(() => {
+    account = new Account()
+  })
 
   it('has a balance of 0 on opening/instantiation', () => {
-    account = new Account()
     expect(account.getBalance()).toBe(0);
   });
 
   describe('#deposit', ()  => {
     it('has balance of 1000 after depositing 1000', () => {
-      account = new Account()
       account.deposit(1000)
       expect(account.getBalance()).toBe(1000);
     });
 
     it('has balance of 1000 after depositing 1000', () => {
-      account = new Account()
       account.deposit(1000)
       expect(account.getBalance()).toBe(1000);
     });
 
     it('has balance of 3000 after depositing 1000 then 2000', () => {
-      account = new Account()
       account.deposit(1000)
       account.deposit(2000)
       expect(account.getBalance()).toBe(3000);
-    });
+    });    
   })
 
 });
