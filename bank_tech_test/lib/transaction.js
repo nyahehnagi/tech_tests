1736434cf8a2c0d47628cd0edf1fe709c01e9168
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-27 20:44:15 +0100

    Created transaction class to manage  transactions

---
+++ bank_tech_test/lib/transaction.js
@@ -0,0 +1,17 @@
+class Transaction {
+  constructor(amount, dateTransacted) {
+    this.amount = amount;
+    this.dateTransacted = dateTransacted
+  }
+
+  getAmount() {
+    return this.amount;
+  }
+
+  getDateTransacted() {
+    return this.dateTransacted;
+  }
+
+}
+
+module.exports = Transaction;
