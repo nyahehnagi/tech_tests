feddcc24ddfe17687307ed321f04a9cb0ed5c280
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-28 22:18:30 +0100

    final test in formatter mocked

--- bank_tech_test/spec/statementformatter.spec.js
+++ bank_tech_test/spec/statementformatter.spec.js
@@ -45,11 +45,21 @@ describe("StatementFormatter", () => {
     });
 
     it("show the statement for 2 deposits and withdrawal", () => {
-      account = new Account();
-      account.deposit(1000, "10-01-2023");
-      account.deposit(2000, "13-01-2023");
-      account.withdraw(500, "14-01-2023");
-      expect(statementFormatter.generateStatement(account)).toBe(
+      Account.getTransactions = jest
+        .fn(Account.getTransactions)
+        .mockImplementation(() => [
+          { amount: -500, dateTransacted: "14-01-2023" },
+          { amount: 2000, dateTransacted: "13-01-2023" },
+          { amount: 1000, dateTransacted: "10-01-2023" }
+        ]);
+
+      Account.getBalance = jest
+      .fn(Account.getBalance)
+      .mockReturnValueOnce(2500)
+      .mockReturnValueOnce(3000)
+      .mockReturnValueOnce(1000)
+      
+      expect(statementFormatter.generateStatement(Account)).toBe(
         "date || credit || debit || balance\r\n14/01/2023 || || 500.00 || 2500.00\r\n13/01/2023 || 2000.00 || || 3000.00\r\n10/01/2023 || 1000.00 || || 1000.00"
       );
     });
