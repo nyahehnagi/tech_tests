ca7d1e3c9a521ed210855f7eb7dada68ac7a15f3
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-30 10:51:18 +0100

    removed where date is passed in tests

--- bank_tech_test/spec/account.spec.js
+++ bank_tech_test/spec/account.spec.js
@@ -38,39 +38,39 @@ describe("account", () => {
 
   describe("#deposit", () => {
     it("has balance of 1000 after depositing 1000", () => {
-      account.deposit(1000, "10-01-2023");
+      account.deposit(1000);
       expect(account.getBalance()).toBe(1000);
     });
 
     it("has balance of 3000 after depositing 1000 then 2000", () => {
-      account.deposit(1000, "10-01-2023");
-      account.deposit(2000, "11-01-2023");
+      account.deposit(1000);
+      account.deposit(2000);
       expect(account.getBalance()).toBe(3000);
     });
 
     it("throws an error if amount is not a number", () => {
       expect(() => {
-        account.deposit("number", "10-01-2023");
+        account.deposit("number");
       }).toThrow("Invalid Amount");
     });
   });
 
   describe("withdraw", () => {
     it("has balance of -500 if withdraw 500", () => {
-      account.withdraw(500, "10-01-2023");
+      account.withdraw(500);
       expect(account.getBalance()).toBe(-500);
     });
 
     it("throws an error if amount is not a number", () => {
       expect(() => {
-        account.withdraw("number", "10-01-2023");
+        account.withdraw("number");
       }).toThrow("Invalid Amount");
     });
   });
 
   describe("printStatement", () => {
     it("checks that generateStatement has been called", () => {
-      account.deposit(1000, "10-01-2023");
+      account.deposit(1000);
       const spy = jest.spyOn(StatementFormatter.prototype, "generateStatement");
       account.printStatement();
       expect(spy).toHaveBeenCalled();
